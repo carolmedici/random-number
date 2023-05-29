a729d3b4442588393b24479059c62fae3caa9d40
carolmedici <carolina.obstetriz@gmail.com>
2023-05-28 21:51:55 -0300

    :sparkles: change theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-
+import { useState, useEffect } from 'react';
 import 'tailwindcss/tailwind.css'
 import './App.css'
 import Random from './assets/Random';
 
 function App() {
- 
+  const [isDark, setIsDark] = useState(false);
 
+  useEffect(() => {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark', isDark);
+  }, [isDark]);
 
   return (
     <>
-    
+      <label className='theme'>
+        <input className='input'
+          type="checkbox"
+          checked={isDark}
+          onChange={e => setIsDark(e.target.checked)}
+        />
+        Change theme
+      </label>
 
-    <Random />
-       
+      <Random />
     </>
   )
 }
 
-export default App
+export default App;
 
 
 
